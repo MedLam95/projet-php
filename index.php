d1e28f600246289d95ee774236a8ca205223ee4c
MedLam95 <lhamouz.mehdi@gmail.com>
2021-05-25 11:32:18 +0200

    Mise en place projet php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!-- Ici on appelle le header -->
+<?php require_once('inc/header.php'); ?>
+
+
+<!-- Ici appelle à la fonction pour compteur de vues -->
+<h1>Salut à tous</h1>
+
+
+<!-- Ici footer -->
+<?php require_once('inc/footer.php'); ?>
